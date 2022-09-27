7f9ed9cc74f7661dd834db7337c4092fb152f20e
jstable1 <jessica.stabler@gmail.com>
2022-09-27 14:55:32 -0600

    updated styling

--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -4,7 +4,9 @@ const SplashPage = () => {
 
     return (
         <section className="splashPage">
-
+            <h1>JESSICA STABLER</h1>
+            <h3>FULL STACK WEB DEVELOPER</h3>
+            <div>insert avatar here</div>
         </section>
     );
 }
