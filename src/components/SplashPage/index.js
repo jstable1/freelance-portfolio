6497044741f45cd89a1a7bd72cc982e597770d40
jstable1 <jessica.stabler@gmail.com>
2022-09-27 16:20:49 -0600

    added react state logic to change hello to a different language every 10 seconds

--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -1,28 +1,51 @@
-import React from "react";
-import MyResume from "../../assets/resume.pdf";
+import React, { Component } from 'react';
+import MyResume from '../../assets/resume.pdf';
 import { GiBee } from 'react-icons/gi';
 import { MdEmail } from 'react-icons/md';
 import { FaFileDownload } from 'react-icons/fa';
 
-const SplashPage = () => {
+const helloArray = ['Hello', 'Bonjour', 'Hola', 'Guten Tag', 'Salve', 'Konnichiwa'];
 
-    return (
-        <section className="splashPage">
-            <div className="splashText">
-                <h1>Hello!</h1> 
-                <h1>I'm Jessica, a Full Stack Web Developer based in Herriman, UT.</h1>
-                <h2>Creative. Detail-Oriented. Organized. Hard-working.</h2>
-                <h3>Technical Skills: HTML/HTML5, CSS, JavaScript, JQuery, Node.js, Express.js, SQL, MongoDB/Mongoose, React, GraphQL, and Python</h3>
-                <h4>
-                    <MdEmail/> <a href="mailto:jessica.stabler@gmail.com">jessica.stabler@gmail.com</a>
-                </h4>
-                <h4>
-                    <span><FaFileDownload/> <a href={MyResume}>resume</a></span>
-                </h4>
-            </div>
-            <div className="splashImg">insert avatar here</div>
-        </section>
-    );
+class SplashPage extends Component {
+
+    constructor() {
+        super();
+        this.state = { arrayIndex: 0 };
+    }
+
+    componentDidMount() {
+        this.timeout = setInterval(() => {
+            let currentId = this.state.arrayIndex;
+            this.setState({ arrayIndex: currentId + 1 });
+        }, 10000);
+    }
+
+    componentDidUnmount() {
+        clearInterval(this.timeout);
+    }
+
+    render() {
+        let changingText = helloArray[this.state.arrayIndex % helloArray.length];
+
+        return (
+            <section className="splashPage">
+                <div className="splashText">
+                    <h1>{changingText}!</h1> 
+                    <h1>I'm Jessica, a Full Stack Web Developer based in Herriman, UT.</h1>
+                    <h2>Creative. Detail-Oriented. Organized. Hard-working.</h2>
+                    <h3>Technical Skills: HTML/HTML5, CSS, JavaScript, JQuery, Node.js, Express.js, SQL, MongoDB/Mongoose, React, GraphQL, and Python</h3>
+                    <h4>
+                        <MdEmail/> <a href="mailto:jessica.stabler@gmail.com">jessica.stabler@gmail.com</a>
+                    </h4>
+                    <h4>
+                        <span><FaFileDownload/> <a href={MyResume}>resume</a></span>
+                    </h4>
+                </div>
+                <div className="splashImg">insert avatar here</div>
+            </section>
+        );
+    }
+
 }
 
 export default SplashPage;
